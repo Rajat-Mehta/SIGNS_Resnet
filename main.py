60e9a622191adec8d7d31c28494d37225469f4e0
Unknown <rajatmehta1992@gmail.com>
2018-08-11 14:48:26 -0400

    added data loading and preprocessing and model train functions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
+import h5py
 import numpy as np
+import keras
 from keras import layers
 from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
 from keras.models import Model, load_model
@@ -11,7 +13,6 @@ import pydot
 from IPython.display import SVG
 from keras.utils.vis_utils import model_to_dot
 from keras.utils import plot_model
-
 from keras.initializers import glorot_uniform
 import scipy.misc
 from matplotlib.pyplot import imshow
@@ -20,6 +21,30 @@ from matplotlib.pyplot import imshow
 import keras.backend as K
 K.set_image_data_format('channels_last')
 K.set_learning_phase(1)
+
+config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 56} )
+sess = tf.Session(config=config)
+keras.backend.set_session(sess)
+
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_signs.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
+
+    test_dataset = h5py.File('datasets/test_signs.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)].T
+    return Y
 
 def identity_block(X, f, filters, stage, block):
     conv_name_base = 'res' + str(stage) + block + '_branch'
@@ -70,3 +95,83 @@ def convolutional_block(X, f, filters, stage, block, s=2):
     X = Activation('relu')(X)
 
     return X
+
+
+def ResNet50(input_shape=(64,64,3), classes=6):
+    X_input = Input(input_shape)
+
+    X = ZeroPadding2D((3,3))(X_input)
+
+    X = Conv2D(64, kernel_size=(7,7), strides=(2,2), name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name='bn_conv1')(X)
+    X = MaxPooling2D((3,3), strides=(2,2))(X)
+
+    X = convolutional_block(X, 3, [64,64,256], 2, 'a', 1)
+    X = identity_block(X, 3, [64,64,256], 2, 'b')
+    X = identity_block(X, 3, [64, 64, 256], 2, 'c')
+
+    X = convolutional_block(X, 3, [128,128,512], 3, 'a', 2)
+    X = identity_block(X, 3, [128,128,512], 3, 'b')
+    X = identity_block(X, 3, [128,128,512], 3, 'c')
+    X = identity_block(X, 3, [128,128,512], 3, 'd')
+
+    X = convolutional_block(X, 3, [256,256,1024], 4, 'a', 2)
+    X = identity_block(X, 3, [256,256,1024], 4, 'b')
+    X = identity_block(X, 3, [256,256,1024], 4, 'c')
+    X = identity_block(X, 3, [256,256,1024], 4, 'd')
+    X = identity_block(X, 3, [256,256,1024], 4, 'e')
+    X = identity_block(X, 3, [256,256,1024], 4, 'f')
+
+    X = convolutional_block(X, 3, [512,512,2048], 5, 'a', 2)
+    X = identity_block(X, 3, [512,512,2048], 5, 'b')
+    X = identity_block(X, 3, [512,512,2048], 5, 'c')
+
+    X = AveragePooling2D((2,2), name='avg_pool')(X)
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc'+str(classes), kernel_initializer=glorot_uniform(seed=0))(X)
+
+    model = Model(inputs=X_input, outputs=X, name='ResNet50')
+    return model
+
+def test_real_time(img_path):
+    img = image.load_img(img_path, target_size=(64, 64))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    print('Input image shape:', x.shape)
+    my_image = scipy.misc.imread(img_path)
+    imshow(my_image)
+    print("class prediction vector [p(0), p(1), p(2), p(3), p(4), p(5)] = ")
+    print(model.predict(x))
+
+model = ResNet50((64,64,3), 6)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+
+# Convert training and test labels to one hot matrices
+Y_train = convert_to_one_hot(Y_train_orig, 6).T
+Y_test = convert_to_one_hot(Y_test_orig, 6).T
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+
+model.fit(X_train, Y_train, epochs=20, batch_size=32)
+
+preds = model.evaluate(X_test, Y_test)
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
+
+test_real_time('images/1.jpeg')
+
+
